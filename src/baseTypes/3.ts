9bc821c1cb8fad2f639e6fb1aafd0d49d66666a6
Andrii Kurshatsov <andreswebit@ukr.net>
2023-10-27 19:18:46 +0300

    Add task 1-5

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,13 @@
-let some:unknown;
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// str = some;
+
+// export { };
+
+let some: unknown;
 some = 'Text';
 let str: string;
-str = some;
+if (typeof some === 'string') str = some;
 
 export {};
--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,13 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+// let union;
+// let literal;
+
+
+let union: string | number;
+union = 'Nadezhda';
+union = 30;
+let literal: 'enable' | 'disable';
+literal = 'enable';
+literal = 'disable';
